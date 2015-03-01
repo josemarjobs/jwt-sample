8783e30dd9d33292fa76c8ad3308442c70a00680
Josemar Magalhaes <josemarmagalhaes@hotmail.com>
2015-03-01 19:42:07 -0300

    Use express jwt

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var faker = require('faker');
 var cors = require('cors');
 var bodyParser = require('body-parser');
 var jwt = require('jsonwebtoken');
+var expressJwt = require('express-jwt');
 
 var jwtSecret = 'sdjfnasdouenadkjfbasdfa/sdf;//?asd';
 
@@ -15,6 +16,7 @@ var user = {
 }
 app.use(cors());
 app.use(bodyParser.json());
+app.use(expressJwt({secret: jwtSecret}).unless({path: ['/login']}));
 
 app.get('/random-user', function(req, res){
 	var user = faker.helpers.userCard();
